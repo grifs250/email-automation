20384de30f58a3bfd70314347b86748986b437a1
Miķelis <mikelissilins25@gmail.com>
2024-06-26 11:20:17 +0300

    first app.js app

--- email-collector/models/Email.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const emailSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Email', emailSchema);
