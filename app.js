164fd330cc54a46c745c9593a3ee631a2482c722
Miķelis <mikelissilins25@gmail.com>
2024-07-03 07:19:52 +0300

    start with packeges

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-//const express = require('express');
-//const app = express;
+const express = require('express');
+const app = express();
+const emails = require('./routes/emails')
 
 
-const port = env.PORT
+// middlewear
 
-console.log(port)
+app.use(express.json())
+
+
+
+// routes
+app.get('/hello', (req, res)=>{
+    res.send("Email automation")
+})
+
+app.use('/api/v1/emails', emails)
+
+// get main.html
+// get tenx.html
+// post name
+
+
+const port = 3000
+
+
+app.listen (port, console.log(`Server is listening on port ${port}...`))
