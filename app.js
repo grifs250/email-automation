2f509dae3671ca5502a6263381088075e2824de2
Miķelis <mikelissilins25@gmail.com>
2024-07-25 16:32:48 +0300

    mvc works, but still no mail sent

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const express = require('express');
-const app = express();
-const emails = require('./routes/emails')
+const morgan =require('morgan');
+const mongoose = require('mongoose')
+const appRoute = require('./routes/route.js')
+require('dotenv').config();
 
+//express app
+const app = express();
+
+// connect to mongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
 
 // middlewear
-
-app.use(express.json())
-
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 // routes
-app.get('/hello', (req, res)=>{
-    res.send("Email automation")
-})
+app.use(appRoute)
 
-app.use('/api/v1/emails', emails)
+app.use((req, res) => {
+    res.render('index', { title: '404' })
+});
 
-// get main.html
-// get tenx.html
-// post name
-
-
-const port = 3000
-
-
-app.listen (port, console.log(`Server is listening on port ${port}...`))
+// listen for requests
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+});
