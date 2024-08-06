890e00c1e36c07294471035651a644aab5feb25d
Miķelis <mikelissilins25@gmail.com>
2024-08-06 12:32:17 +0300

    beck to normal front end valitation

--- email-collector/routes/route.js
+++ email-collector/routes/route.js
@@ -8,15 +8,15 @@ const router = express.Router();
 router.get('/', appController.index_get);
 router.get('/tnx', appController.tnx_get);
 router.post('/tnx', [
-    // // Validate and sanitize inputs "provides additional security"
-    // body('name')
-    //     .trim()
-    //     .isLength({ min: 2 }).withMessage('Vārdam ir jābūt vismaz 2 rakstzīmju garumā')
-    //     .isLength({ max: 100 }).withMessage('Vārds nedrīkst būt garāks par 100 rakstzīmēm')
-    //     .escape(),
-    // body('email')
-    //     .isEmail().withMessage('Lūdzu ievadiet derīgu e-pasta adresi')
-    //     .normalizeEmail(),
+    // Validate and sanitize inputs "provides additional security"
+    body('name')
+        .trim()
+        .isLength({ min: 2 }).withMessage('Vārdam ir jābūt vismaz 2 rakstzīmju garumā')
+        .isLength({ max: 100 }).withMessage('Vārds nedrīkst būt garāks par 100 rakstzīmēm')
+        .escape(),
+    body('email')
+        .isEmail().withMessage('Lūdzu ievadiet derīgu e-pasta adresi')
+        .normalizeEmail(),
 ], appController.tnx_post);
 
 module.exports = router;
