d94c01fd123a00e1a8ed0c1e0be0c36f49b7d50c
Miķelis <mikelissilins25@gmail.com>
2024-08-07 08:25:33 +0300

    injectspeed insights

--- email-collector/public/js/main.js
+++ email-collector/public/js/main.js
@@ -1,22 +1,28 @@
 
 //I realized that hovering on icon is much better way to displaying info
 // Function to toggle the visibility of the tooltip
-function handleInfoIconClick() {
-    var tooltip = document.getElementById('tooltip');
-    if (tooltip) {
-        tooltip.classList.toggle('visible');
-    }
-}
+// function handleInfoIconClick() {
+//     var tooltip = document.getElementById('tooltip');
+//     if (tooltip) {
+//         tooltip.classList.toggle('visible');
+//     }
+// }
 
-// Function to set up event listeners
-function setupEventListeners() {
-    var infoIcon = document.querySelector('.info-icon');
-    if (infoIcon) {
-        infoIcon.addEventListener('click', handleInfoIconClick);
-    }
-}
+// // Function to set up event listeners
+// function setupEventListeners() {
+//     var infoIcon = document.querySelector('.info-icon');
+//     if (infoIcon) {
+//         infoIcon.addEventListener('click', handleInfoIconClick);
+//     }
+// }
 
-// Ensure the DOM is fully loaded before attaching event listeners
-document.addEventListener('DOMContentLoaded', function() {
-    setupEventListeners();
-});
+// // Ensure the DOM is fully loaded before attaching event listeners
+// document.addEventListener('DOMContentLoaded', function() {
+//     setupEventListeners();
+// });
+
+
+
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
