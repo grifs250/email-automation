170846191ef9c866e33c03ff1425c42ad24f7dd5
Miķelis <mikelissilins25@gmail.com>
2024-08-21 05:21:52 +0300

    implemented  code for icon button on phones

--- email-sender/public/js/main.js
+++ email-sender/public/js/main.js
@@ -24,3 +24,18 @@ function confirmDeletion(recordId) {
         });
     }
 }
+
+// // Function to toggle between light and dark mode
+// function toggleTheme() {
+//     document.body.classList.toggle('dark-mode');
+//     const newTheme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+//     localStorage.setItem('theme', newTheme);
+// }
+
+// // Initialize the theme based on localStorage
+// (function() {
+//     const currentTheme = localStorage.getItem('theme') || 'light';
+//     if (currentTheme === 'dark') {
+//         document.body.classList.add('dark-mode');
+//     }
+// })();
